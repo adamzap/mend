5b992de623cac9dfee5ce0f7b67173e60e7d5ce3
Adam Zapletal <adamzap@gmail.com>
2011-03-03 23:58:07 -0600

    Removed configuration options

--- mend.py
+++ mend.py
@@ -5,26 +5,22 @@ import shutil
 import jinja2
 import markdown
 
-try:
-    EXTS, DIRS = [l.strip().split(' ') for l in open('mend.conf').readlines()]
-except IOError:
-    EXTS, DIRS = [], []
+IGNORE = ('base.html', 'deploy')
+
+base_tmpl = jinja2.Template(open('base.html').read())
+
+md = markdown.Markdown()
 
 try:
     shutil.rmtree('deploy')
 except OSError:
     pass
 
-base_tmpl = jinja2.Template(open('base.html').read())
-
 os.mkdir('deploy')
 
-md = markdown.Markdown()
+ls = [x for x in os.listdir('.') if not x[0] == '.' and x not in IGNORE]
 
-for page_name in os.listdir('.'):
-    if page_name.split('.')[-1] not in EXTS or page_name == 'base.html':
-        continue
-
+for page_name in [x for x in ls if os.path.isfile(x)]:
     title = page_name.split('.')[0].replace('_', ' ').title()
     body = open(page_name).read()
 
@@ -36,5 +32,5 @@ for page_name in os.listdir('.'):
     rendered_file.write(base_tmpl.render(locals()))
     rendered_file.close()
 
-for dir in DIRS:
+for dir in [x for x in ls if os.path.isdir(x)]:
     shutil.copytree(dir, os.path.join('deploy', dir))
